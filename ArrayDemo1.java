3f9a9b5b7cb0a97753c4ae5dc8d126d1eac3984d
Rodnieeeeeeeeeeee <rodnieespeso075@gmail.com>
2024-09-06 18:29:41 +0800

    Add files via upload

---
+++ ArrayDemo1.java
@@ -0,0 +1,11 @@
+public class ArrayDemo{
+	public static void main(String[] args) {
+		
+		//datatype[] variableName;
+		
+		//elements = 5
+		//index 0 1 2 3 4
+		String[] studentName = {"Ynara","Chiara","Ynaro","Zarah","Carlo"};
+		
+		for(byte i= 0; <=studentName.length-1;i++){
+		    System.out.println(studentName[i] + "\t" +i);
